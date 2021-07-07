848bf84ba9c000ec780e1bb579d48f03eb9e5b57
vikram <vikram@rakuten.com>
2021-07-07 22:45:22 +0530

    Update ec2-userdata.sh

--- AWS/ec2-userdata.sh
+++ AWS/ec2-userdata.sh
@@ -1,4 +1,13 @@
+# Example - 01
+
 #! /bin/bash
 echo "Updating the instance" >> /tmp/startup.log
 sudo yum update -y
 echo $HOSTNAME >> /tmp/startup.log
+
+# Example - 02
+
+#! /bin/bash
+sudo yum install -y httpd 
+echo "Response from Host `hostname`" > /var/www/html/index.html
+sudo systemctl enable --now httpd
